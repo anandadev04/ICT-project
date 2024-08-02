f8f0fbff83435ed732dc2b99cf12b243cd01e703
AfrinaFarook <farooqafrina28@gmail.com>
2024-08-02 18:13:11 +0530

    newtablecomment

--- backend/model/commentData.js
+++ backend/model/commentData.js
@@ -1,15 +1,8 @@
 const mongoose=require('mongoose')
 const commentSchema = new mongoose.Schema({
     eventName: { type: String, required: true },
-    // userName:{type}
-    description: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    timings: { type: String, required: true },
-    days: { type: String, required: true },
-    venue: { type: String, required: true },
-    likes: { type: Number, required: true},
-    picture: { type: String, required: true } 
+    userName:{type: String,required: true},
+    comments: { type: String, required: true },
 });
 const commentData=mongoose.model('likes',commentSchema);
 module.exports=EventData
