877621e17a137c5dad9822289dd1c59ac547a454
Anandadev <adevd2004@gmail.com>
2024-08-04 05:28:55 +0530

    more changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginSignup from './Components/LoginSignup';
 import Home from './Components/Home';
-import Profile from './Components/Profile'
-import EventList from './Components/EventList'
-import Register from './Components/Register'
+import Profile from './Components/Profile';
+import EventList from './Components/EventList';
+import Register from './Components/Register';
 import Eventdetails from './Components/Eventdetails';
+import TermsAndConditions from './Components/Termsandconditions'; // Import the component
 
-
+const NotFound = () => {
+  return <h2>404 - Page Not Found</h2>;
+};
 
 function App() {
   return (
@@ -15,11 +18,13 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginSignup />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />}/>
+        <Route path="/profile" element={<Profile />} />
         <Route path="/event" element={<EventList />} />
         <Route path="/register" element={<Register />} />
         <Route path="/eventdetails/:eventId" element={<Eventdetails />} />
-        <Route path="/regback" element={<EventList />}/>
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} /> {/* New route */}
+        <Route path="/regback" element={<EventList />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
