92eaee38181542830e340990e44a0345e1d1c5c5
AmrithaManoj123 <amrithamanoj.b22cs2116@mbcet.ac.in>
2024-08-04 04:02:50 +0530

    neww

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo1.png'; // Adjust the path to your logo image
+
 const Navbar = () => {
   return (
     <header className='header'>
-      <a href="/" className="nav-logo">Logo</a>
+      <a href="/" className="nav-logo">
+        <img src={logo} alt="Logo" className="logo-image" />
+      </a>
 
       <nav className="navbar">
-      <Link to="/home">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/event">Events</Link>
-      <Link to="/">Log Out</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/event">Events</Link>
+        <Link to="/">Log Out</Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
