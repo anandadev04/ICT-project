6b638a0ed83ab56e1b1aa50021798de27c0564cb
AfrinaFarook <farooqafrina28@gmail.com>
2024-08-04 06:19:43 +0530

    wallchangehome

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,4 +1,3 @@
-// Home.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Home.css';
@@ -7,15 +6,15 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, colors } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Navbar from './Navbar';
 
-const GradientCardContent = styled(CardContent)(({ theme }) => ({
-  position: 'relative',
-  padding: theme.spacing(1),
-  background: 'linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))',
-}));
+// const GradientCardContent = styled(CardContent)(({ theme }) => ({
+//   position: 'relative',
+//   padding: theme.spacing(1),
+//   background: 'linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))',
+// }));
 
 const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
   position: 'relative',
@@ -51,6 +50,7 @@ const Home = () => {
     const firstSentence = text.split(sentenceEnd)[0].trim();
     return firstSentence + (firstSentence ? '.' : ''); // Add period if there was text
   };
+
   const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
@@ -59,12 +59,36 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="content">
-        <h1 className="heading">Upcoming Events</h1>
+      <div className="navbar-container">
+       
+        <div className="banner-container">
+  <img 
+    src="/src/assets/wall3.jpg" 
+    alt="Banner" 
+    className="banner-image" 
+  />
+  <img 
+    src="/src/assets/logo.png" 
+    alt="Overlay" 
+    className="overlay-image" 
+  />
+</div>
+        
+          <h1 className="heading">      About Us</h1> 
+          <div  style={{paddingLeft:"18%",paddingRight:"18%"}}><p style={{ color: 'black' ,textAlign: 'center'}}>
+          Welcome to [Your Company Name] – your go-to destination for discovering and registering for the latest and most exciting events!
+          We are dedicated to bringing you a diverse range of events that cater to every interest and occasion, ensuring you never miss out on an opportunity to connect, celebrate, and experience something new.
+          Our platform offers a seamless way to explore upcoming events, from community gatherings and professional workshops to concerts and social mixers. Here’s what we offer:
+          EventListings: Browse through a comprehensive list of upcoming events tailored to your interests. Each event page provides detailed information, including dates, locations, schedules, and more.
+Event Details: Get all the essential information you need to make informed decisions about the events you want to attend.
+Easy Registration: Secure your spot with our user-friendly registration process. Sign up and pay for tickets directly through our platform for a hassle-free experience.
+At [Your Company Name], we are passionate about helping you stay engaged and make the most out of every event. Whether you’re looking to attend a local festival, a professional seminar, or an exclusive networking event, we’ve got you covered.
+
+Explore our event calendar today and be part of the exciting experiences waiting for you!</p></div>
         <div className="cards-container">
           {events.map((event) => (
-            <Card key={event._id} sx={{ maxWidth: 320, borderRadius: "5%", overflow: 'hidden', backgroundColor: '#333', color: 'white' }}>
-              <GradientCardContent>
+            <Card key={event._id} sx={{ maxWidth: 320, overflow: 'hidden', backgroundColor: '#333', color: 'white' }}>
+              {/* <GradientCardContent> */}
                 <CardActionArea onClick={() => handleCardClick(event._id)}>
                   <StyledCardMedia
                     component="img"
@@ -80,11 +104,11 @@ const Home = () => {
                       Start Date: {formatDate(event.startDate)}
                     </Typography>
                     <Typography variant="body2" color="white">
-                    {getFirstSentence(event.description)}
-                  </Typography>
+                      {getFirstSentence(event.description)}
+                    </Typography>
                   </CardContent>
                 </CardActionArea>
-              </GradientCardContent>
+              {/* </GradientCardContent> */}
             </Card>
           ))}
         </div>
@@ -94,5 +118,3 @@ const Home = () => {
 }
 
 export default Home;
-
-
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,18 +5,20 @@ import logo from '../assets/logo1.png'; // Adjust the path to your logo image
 
 const Navbar = () => {
   return (
-    <header className='header'>
-      <a href="" className="nav-logo">
-        <img src={logo} alt="Logo" className="logo-image" />
-      </a>
+    <div className='navbar-container'>
+      <header className='header'>
+        <a href="" className="nav-logo">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </a>
 
-      <nav className="navbar">
-        <Link to="/home">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/event">Events</Link>
-        <Link to="/">Log Out</Link>
-      </nav>
-    </header>
+        <nav className="navbar">
+          <Link to="/home">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/event">Events</Link>
+          <Link to="/">Log Out</Link>
+        </nav>
+      </header>
+    </div>
   );
 };
 
